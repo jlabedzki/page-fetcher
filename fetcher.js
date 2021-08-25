aac9452c78d34fe49f299ff47aac8d9ed1849155
Johnny <johnnylabedzki@hotmail.com>
2021-08-24 19:01:47 -0700

    Fixed some linting errors on fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -10,8 +10,9 @@ const rl = readline.createInterface({
 const url = process.argv[2];
 const dest = process.argv[3];
 
+// Function found here https://thispointer.com/javascript-check-if-string-is-url/
 const isValidUrl = string => {
-  const matchpattern = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)$/gm;
+  const matchpattern = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)$/gm;
   return matchpattern.test(string);
 };
 
@@ -30,16 +31,18 @@ const fetcher = (url, dest) => {
   if (fs.existsSync(dest)) {
 
     rl.question(`Warning! File already exists.
-    Do you wish to continue? Y/N: `, answer => {
+    Do you wish to overwrite? Y/N: `, answer => {
 
       if (answer === 'y' || answer === 'Y') {
         request(url, (error, response, body) => {
-          console.log('error:', error); // Print the error if one occurred
+          if (error) console.log('URL request error:', error); // Print the error if one occurred
           console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
           const content = body;
 
-          fs.writeFile(dest, content, { flag: 'a+' }, err => { });
+          fs.writeFile(dest, content, err => {
+            if (err) console.log('File write error:', err);
+          });
           console.log(`Downloaded and saved ${content.length} bytes to ${dest}`);
         });
         rl.close();
